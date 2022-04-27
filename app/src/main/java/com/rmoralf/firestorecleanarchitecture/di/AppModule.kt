c07ce8abe0dbd272a0f2cb90a6cf6defcd8f4ad7
Rodrigo Moral <rmoralf@gmail.com>
2022-04-27 18:56:00 +0200

    Add @ExperimentalCoroutinesApi notation

--- app/src/main/java/com/rmoralf/firestorecleanarchitecture/di/AppModule.kt
+++ app/src/main/java/com/rmoralf/firestorecleanarchitecture/di/AppModule.kt
@@ -10,8 +10,10 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @Module
+@ExperimentalCoroutinesApi
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
